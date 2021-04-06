9cf099eba9eb1c59eecc79adcab0d73a457cf4b9
melgibson <barbocz.attila@gmail.com>
2021-04-06 18:26:42 +0200

    cc

--- get_stock_price.py
+++ get_stock_price.py
@@ -7,9 +7,11 @@ data, meta_data = ts.get_daily_adjusted(symbol='AAPL' ,outputsize='full')
 data=data.rename(columns={"1. open": "Open", "2. high": "High","3. low": "Low","4. close": "Close","5. adjusted close": "Adjusted Close","6. volume": "Volume",})
 data=data.drop(['7. dividend amount','8. split coefficient'], axis = 1)
 
-data['PM_change'] = data.Open - data['Adjusted Close'].shift(-1)
-data['Day_change'] = data['Adjusted Close'] - data.Open
-data['%_Change'] = data['PM_change'] / data.Open.shift(-1)
+# data['Adjusted Close']=data['Close']
+data['PM_change']=0.0
+# data['PM_change'] = data['Adjusted Close'] - data['Adjusted Close'].shift(-1)
+data['Day_change'] = data['Close'] - data.Open
+data['%_Change'] = data['PM_change'] / data['Adjusted Close'].shift(-1)
 data['Date']=data.index
 
 print(data.info())
