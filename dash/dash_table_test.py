c443745f18b161e1a8320b786d460dbbe9e38f3c
Barbocz Attila <barbocz.attila@gmail.com>
2021-04-18 19:29:54 +0200

    Első nagy rendberakás

--- dash/dash_table_test.py
+++ dash/dash_table_test.py
@@ -5,6 +5,8 @@ from joblib import dump, load
 import time
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 import plotly.express as px
 start_time = time.time()
 
@@ -12,9 +14,32 @@ start_time = time.time()
 df=load('../data/processed_sentiment_AAPL.pkl')
 df['Date']=pd.to_datetime(df['Date'])
 
-df=df[df['Date']=='2021-02-11']
+current_day='2021-02-11'
+ticker='AAPL'
+df=df[df['Date']==current_day]
+
+total_sentiment=len(df)
 
 value_counts = df['Combined sentiment'].value_counts()
+
+bearish_bullish_ratio=(value_counts['Bearish']/value_counts['Bullish'])
+
+ema_value=2.023
+
+signal='BUY'
+
+row1 = html.Tr([html.Td("Date:"), html.Td(current_day)])
+row2 = html.Tr([html.Td("Total sentiment:"), html.Td(total_sentiment)])
+row3 = html.Tr([html.Td("Bearish / Bullish ratio:"), html.Td('{:.3f}'.format(bearish_bullish_ratio))])
+row4 = html.Tr([html.Td("EMA value:"), html.Td(ema_value)])
+row5 = html.Tr([html.Td("Signal:"), html.Td(signal)])
+row6 = html.Tr([html.Td(""), html.Td("")])
+
+
+table_body = [html.Tbody([row1, row2, row3, row4, row5,row6])]
+
+table = dbc.Table( table_body, bordered=True)
+
 
 
 data = df.to_dict('rows')
@@ -36,10 +61,20 @@ columns = [{
 
 app = dash.Dash(__name__)
 
+@app.callback(
+    Output('refresh_button', 'active'),
+    Input('refresh_button', 'n_clicks'),
+          )
+def update_output(n_clicks):
+    print( 'the button has been clicked {} times'.format(      n_clicks    ))
+    return True
+
+
+
 
 app.layout = html.Div([
     html.Div([
-    html.Div(style={'width': '31%', 'border': '5px outset red','display': 'inline-block'},children=[dt.DataTable(data=data, columns=columns, id='table',
+    html.Div(style={'width': '32%', 'border': '5px outset red','display': 'inline-block'},children=[dt.DataTable(data=data, columns=columns, id='table',
 
 
                                              style_header={'fontWeight': 'bold', 'backgroundColor': 'black',
@@ -77,11 +112,15 @@ app.layout = html.Div([
 
                                                       )
              )],
-                style={'width': '31%','border': '5px outset red',  'float': 'right','display': 'inline-block'}),
-        html.Div(
-            children=['INFO'],
-            style={'width': '31%', 'border': '5px outset red', 'display': 'inline-block'})
-    ])
+                style={'width': '32%','border': '5px outset red',  'float': 'right','display': 'inline-block'}),
+        html.Div(children=[
+                        html.Div(className="card-body", children=[table,dbc.Button("Refresh", id="refresh_button", n_clicks=0)
+
+            ])
+        ],
+
+            style={'width': '32%', 'border': '5px outset red', 'float': 'right','display': 'inline-block'})
+        ])
     ])
 #                 ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
 # )
